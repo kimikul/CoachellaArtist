492db86980ba9f4b544a0e4361ab466288250127
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2014-03-30 13:05:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoachellaArtist::Application.config.secret_key_base = 'e879bee0fb533b163e9a1c9b82369800ca4973b14d3c5e31dce772f5b3559b16f1867bee19f686637dc8157900db14e4fb9ee2d312fa360d7c92bee34b01ff2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoachellaArtist::Application.config.session_store :cookie_store, key: '_CoachellaArtist_session'
